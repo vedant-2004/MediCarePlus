59e5d7f5608cf54df59a091026a2147703cbffc9
Vedant Mahajan <vedantgmahajan@gmail.com>
2026-02-17 18:45:52 +0530

    Initial Spring Boot project structure - Day 1

---
+++ src/main/java/com/Medicare/medicareplus/MedicareplusApplication.java
@@ -0,0 +1,13 @@
+package com.Medicare.medicareplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicareplusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicareplusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Medicare/medicareplus/MedicareplusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Medicare.medicareplus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicareplusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
